b043fd24663e566ede83c22976a21bce4046d1dd
uehara-koudai <uehara@mns.k.u-tokyo.ac.jp>
2023-09-16 03:44:42 +0900

    投稿一覧表示とぺジネーション

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;//この部分がDBのpostsテーブルからPostモデルにデータを渡している
+    
+    public function getByLimit(int $limit_count = 10)
+    {
+        // updated_at(更新した時刻)で降順(DESC)に並べ，limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+    
+    
+    public function getPaginateByLimit(int $limit_count =10)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
 }
